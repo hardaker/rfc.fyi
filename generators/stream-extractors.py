81e1c932ffb9fb38e22df652802f1a45c75440c1
Wes Hardaker <opensource@hardakers.net>
2021-03-11 05:37:59 -0800

    make stream-extractors update a json

--- generators/stream-extractors.py
+++ generators/stream-extractors.py
@@ -1,15 +1,63 @@
+"""Reads an RFC index and outputs a list of RFCs per stream"""
+
 import json
 from collections import defaultdict
 from rfcjson import get_json
 
+from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter, FileType
+import sys
 
-# open the rfc index
-json_records = get_json(open("rfc-index.xml"))
 
-stream_rfcs = defaultdict(list)
+def parse_args():
+    parser = ArgumentParser(formatter_class=ArgumentDefaultsHelpFormatter,
+                            description=__doc__,
+                            epilog="Example Usage: stream-extractors.py -i rfc-index.xml -p")
 
-for item in json_records:
-    tag = "stream:" + json_records[item]['stream']
-    stream_rfcs[tag].append(item)
+    parser.add_argument("-i", "--rfc-index", default="rfc-index.xml", type=str,
+                        help="The rfc-index.xml file to read in")
 
-print(json.dumps(stream_rfcs, indent=1, sort_keys=True))
+    parser.add_argument("-p", "--print", action="store_true",
+                        help="Print the results to stdout")
+
+    parser.add_argument("-m", "--merge-into", default=None, type=str,
+                        help="Merge results into an existing (tags.json) file")
+
+    args = parser.parse_args()
+    return args
+
+
+def get_stream_data(filename):
+    # open the rfc index
+    json_records = get_json(open(filename))
+
+    stream_rfcs = defaultdict(list)
+
+    for item in json_records:
+        tag = "stream:" + json_records[item]['stream']
+        stream_rfcs[tag].append(item)
+
+    return stream_rfcs
+
+
+def merge_into(stream_data, filename):
+    with open(filename) as fh:
+        existing_data = json.load(fh)
+    existing_data.update(stream_data)
+
+    with open(filename, "w") as fh:
+        json.dump(existing_data, fh, indent=4, sort_keys=True)
+
+
+def main():
+    args = parse_args()
+    stream_data = get_stream_data(args.rfc_index)
+
+    if args.print:
+        print(json.dumps(stream_data, indent=1, sort_keys=True))
+
+    if args.merge_into:
+        merge_into(stream_data, args.merge_into)
+
+
+if __name__ == "__main__":
+    main()
